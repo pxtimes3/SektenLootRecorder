2919f0222212f84cd7ac6c9334ca5cb66e19d322
Magnus Sandell <m@mamoco.se>
2019-12-20 23:22:11 +0100

    testing ace window

--- Init.lua
+++ Init.lua
@@ -1,6 +1,6 @@
 -- Im here to add some bytes
 
-SektenLR = LibStub("AceAddon-3.0"):NewAddon("Sekten Loot Recorder", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0" )
+SektenLR = LibStub("AceAddon-3.0"):NewAddon("Sekten Loot Recorder", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceSerializer-3.0", "AceGUI-3.0" )
 SLR = {}
 
 slr_version = "0.1"
